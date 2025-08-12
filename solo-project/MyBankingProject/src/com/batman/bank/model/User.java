fa4909fc42b40ca13aed672ba9f07d9b3a64f4a5
lightning47 <Vignesh112847@gmail.com>
2025-08-12 20:45:15 +0530

    Update User.java

--- solo-project/MyBankingProject/src/com/batman/bank/model/User.java
+++ solo-project/MyBankingProject/src/com/batman/bank/model/User.java
@@ -1,16 +1,16 @@
 package com.batman.bank.model;
 
-import java.util.Scanner;
-
 public class User {
     private String username;
     private String password;
     private double balance;
+    private String mail;
 
-    public User(String n, String p, double b) {
+    public User(String n, String m, String p, double d) {
         this.username = n;
+        this.mail = m;
         this.password = p;
-        this.balance = b;
+        this.balance = d;
     }
     public String getusername() {
         return username;
@@ -21,10 +21,12 @@ public class User {
     public double getbalance() {
         return balance;
     }
+    public String getmail() {
+        return mail;
+    }
     
     public void deposit(double amount) {
-        try (Scanner ob = new Scanner(System.in)) {
-            int c=12;
+  
             if(amount <=0) {
                 return;   //handle it in the web aswell
             }
@@ -35,24 +37,16 @@ public class User {
                 System.out.println("amount has been credited");
             }
             balance = balance + amount;
-            while(c!=1 || c!=0) {
-            System.out.println("Would you like to show balance? 1- show 0- not");
-            c = ob.nextInt();
-            switch (c) {
-                case 1 -> System.out.println("New balance: " + balance);
-                case 0 -> System.out.println("Thank you");
-                default -> System.out.print("Invalid");
-            }
-   }
+
         }
     
-    }
+    
     public boolean  withdraw(double amount) {
         if(amount>balance)  { //handle amount<=0 on the web
             System.out.println("Insuffcient balance");
             return false;
         }
-        if(amount > balance/65) {
+        if(amount > balance/457) {
             //this function is still under construction
         }
 
